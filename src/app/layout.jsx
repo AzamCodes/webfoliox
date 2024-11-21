816733224be49473a554e081175374bee96acb35
AzamCodes <azam.shaikh9105@gmail.com>
2024-11-21 12:15:43 +0530

    add favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,13 +7,16 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Portfolio Website",
   description: "Personal Portfolio Website",
+  icons: {
+    icon: "/favicon.ico", // Reference to your favicon
+  },
 };
 
 export default function Layout({ children }) {
   return (
     <html
       lang="en"
-      className="h-full bg-neutral-950 text-base antialiased text-neutral-100 "
+      className="h-full bg-neutral-950 text-base antialiased text-neutral-100"
     >
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
